e2be803d8048b0520702024ee5fd13952d38dad9
NokibulAlam <nokibul.alam02@gmail.com>
2022-06-30 01:12:07 +0600

    item and card show complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
-function App() {
+
+const App = () => {
   const expenses = [
     {
       id: 'e1',
@@ -19,14 +20,19 @@ function App() {
       title: 'Food',
       amount: 999,
       date: new Date(2020, 3, 30),
+    }, 
+    {
+      id: 'e4',
+      title: 'Tution Fees',
+      amount: 1800,
+      date: new Date(2020, 5, 16),
     }
   ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title = {expenses[0].title} amount = {expenses[0].amount} date = {expenses[0].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[1].title} amount = {expenses[1].amount} date = {expenses[1].date}></ExpenseItem>
-      <ExpenseItem title = {expenses[2].title} amount = {expenses[2].amount} date = {expenses[2].date}></ExpenseItem>
+      <Expenses items = {expenses}></Expenses>
     </div>
   );
 }
